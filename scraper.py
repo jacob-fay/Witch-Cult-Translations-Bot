7e8362cd580820cba83b63ed720e0896da482d5e
jacob-fay <71296174+jacob-fay@users.noreply.github.com>
2026-04-07 10:21:13 -0400

    added scapper for witchcult

---
+++ scraper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+class Chapter:
+    ___slots__ = ("chapterName","link")
+    def __init__(self,chapterName:str,link:str,date:str):
+        self.chapterName =chapterName
+        self.link = link
+        self.date = date
+    def _dateToInt(self) -> tuple[int, int, int,int,int,int]:
+        big,small = self.date.split("T")
+        return tuple(map(int, big.split("-")+small.split("+")[0].split(":")))
+    def __gt__(self, other):
+        return self._dateToInt() > other._dateToInt()
+    def __eq__(self, other):
+        return  self.date == other.date
+    def __str__(self):
+        return f'Chapter Name: {self.chapterName}, link: {self.link}, date: {self.date}'
+    @staticmethod
+    def readFromJson(filename:str) -> "Chapter":
+        with open (filename, 'r') as file:
+            chapterInfo = json.loads(file.readline())
+            return Chapter(**chapterInfo)
+    @staticmethod
+    def fetchLastedChapters() -> list["Chapter"]:
+        site = requests.get("https://witchculttranslation.com/")
+        bs = BeautifulSoup(site.text,("html.parser"))
+        recentChapters = [x for x in bs.find_all("li",class_="rpwe-li rpwe-clearfix")]
+        li: list[Chapter] = []
+        for chapter in recentChapters:
+            link = chapter.find("a").get("href")
+            name = chapter.find("a").getText()
+            date = chapter.find("time").get("datetime")
+            li.append(Chapter(name,link,date))
+        return li
+    def writeToJson(self,filename:str):
+        with open (filename,"w") as file:
+            file.write(json.dumps({"chapterName": self.chapterName,"link":self.link,"date":self.date}))
+def main():
+    chapterlist: list[Chapter] = Chapter.fetchLastedChapters()
+    chapterlist.sort(reverse=True)
+    chapterlist[0].writeToJson("lastestChapter.json")
+
+    print(Chapter.readFromJson("lastestChapter.json"))
+if (__name__ == "__main__"):
+    main()
+
+
